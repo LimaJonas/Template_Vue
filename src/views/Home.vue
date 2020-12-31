42cb0e831ec6ff5076752f3c46f7a1dfc483f047
Jonas Lima <jonaslima015@gmail.com>
2020-12-31 18:04:17 -0300

    adicionado services e footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,23 @@
     <div class="app">
         <Navbar />
         <Home />
+        <Services />
+        <Footer />
     </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
 import Home from "@/components/Home";
+import Services from "@/components/Services";
+import Footer from "@/components/Footer";
 
 export default {
     components: {
         Navbar,
         Home,
+        Services,
+        Footer
     }
 }
 </script>
