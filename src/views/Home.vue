2d4a063094ea4cfe30d86b9daca0fec6bec5bde1
Jonas Lima <jonaslima015@gmail.com>
2020-12-31 17:40:06 -0300

    adicionado router

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app">
+        <h2>Teste</h2>
+        <router-link
+            tag="button"
+            :to="{ name: 'HelloWorld' }"
+          >
+            Hello
+          </router-link>  
+    </div>
+</template>
