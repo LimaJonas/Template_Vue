7bc97833c7921cd9e2f4b7d00f6e850b95b0b0b6
Jonas Lima <jonaslima015@gmail.com>
2021-01-03 14:38:02 -0300

    alterado local da home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,6 @@
 <template>
+<div>
+    <Navbar />
     <header class="masthead">
         <div class="container">
             <div class="masthead-subheading">Welcome To Our Studio!</div>
@@ -6,7 +8,21 @@
             <router-link :to="{ name: 'Services' }"><a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Tell Me More</a></router-link>
         </div>
     </header>
+    <Footer />
+</div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from '@/components/Footer.vue';
+
+export default {
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
 
 <style>
 header.masthead {
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -49,5 +49,4 @@ export default {
         Footer
     }
 }
-        Footer
 </script>
